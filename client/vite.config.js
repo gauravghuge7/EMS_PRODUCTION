303297182403047fe245f77b8d04401067bdfdff
Gaurav Ghuge <guduughuge7@gmail.com>
2024-10-09 16:32:00 +0530

    update like production

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,16 +4,16 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
 
-  server: {
-    proxy:  {
-      '/api/v1':  {
+  // server: {
+  //   proxy:  {
+  //     '/api/v1':  {
       
-        target: 'http://13.202.64.146:5200',  // Fixed the missing slash
-        changeOrigin: true,  
+  //       target: 'http://13.202.64.146:5200',  // Fixed the missing slash
+  //       changeOrigin: true,  
         
-      }
-    }
-  },
+  //     }
+  //   }
+  // },
 
   plugins: [react()],
 
